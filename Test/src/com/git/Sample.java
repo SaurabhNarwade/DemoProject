4d06f9bdd8821493da71ea0762283eecf8749546
SaurabhNarwade <saurabhnarwade9156@gmail.com>
2023-04-25 13:01:02 +0530

    print the value of a and b

--- Test/src/com/git/Sample.java
+++ Test/src/com/git/Sample.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 public class Sample{
 	public void addition(int a,int b) {
 		int c = a+b;
+		System.out.println("value of a" +a);
+		System.out.println("value of b" +b);
 		System.out.println(c);
 	}
 	public static void main(String[] args) {
