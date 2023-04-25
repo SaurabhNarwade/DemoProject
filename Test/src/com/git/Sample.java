20f3628ecff9ff54e2a918878a722a3c384698b4
SaurabhNarwade <saurabhnarwade9156@gmail.com>
2023-04-25 12:06:48 +0530

    added the printing 1 to 100

--- Test/src/com/git/Sample.java
+++ Test/src/com/git/Sample.java
@@ -2,7 +2,13 @@ package com.git;
 import java.util.Scanner;
 
 public class Sample{
+	public void addition(int a,int b) {
+		int c = a+b;
+		System.out.println(c);
+	}
 	public static void main(String[] args) {
+		Sample S = new Sample();
+		S.addition(10, 40);
 		
 	}
 
